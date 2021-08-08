51950deee2b44111f9d1d6906e35304ad2df88c2
Alzter Aquino <alztr2908@gmail.com>
2021-08-08 19:52:31 +0800

    initial async

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const baseURL =
+  "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon";
+
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getData(baseURL);
+      await loadData(response.data[0]);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  async function getData(url) {
+    return new Promise((resolve, reject) => {
+      axios.get(url).then((res) => {
+        resolve(res.data);
+      });
+    });
+  }
+
+  async function loadData(data) {
+    setPokemon(data);
+  }
+
   return (
     <div>
-      <h1>asdsd</h1>
+      {loading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <>
+          <img src={pokemon.card_images[0].image_url} alt='#' />
+          <h1>{pokemon.atk}</h1>
+          {pokemon.card_sets.map((set) => (
+            <div key={set.set_code}>
+              <h2>{set.set_name}</h2>
+              <p>{set.set_price}</p>
+              <p>{set.set_rarity}</p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
