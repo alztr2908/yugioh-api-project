1878e369c0275eabb039da551c4b560f28f87d44
Alzter Aquino <alztr2908@gmail.com>
2021-08-13 17:27:53 +0800

    add nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,36 @@
-import { Layout } from "antd";
+import { Anchor, Button } from "antd";
+import logo from "./logo/logo.png";
+import "./stylesheets/Navbar.css";
+import { AiFillHome } from "react-icons/ai";
+import { BiCabinet } from "react-icons/bi";
+import { FaRandom } from "react-icons/fa";
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Link } = Anchor;
 
 function Navbar() {
   return (
-    <div>
-      <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
+    <div className="background">
+      <img src={logo} alt="img" />
+      <div className="list-background">
+        <Button className="list-background-button">
+          <AiFillHome
+            style={{ position: "relative", top: "2px", marginRight: "10px" }}
+          />
+          Home
+        </Button>
+        <Button className="list-background-button">
+          <BiCabinet
+            style={{ position: "relative", top: "2px", marginRight: "10px" }}
+          />
+          Directory
+        </Button>
+        <Button className="list-background-button">
+          <FaRandom
+            style={{ position: "relative", top: "2px", marginRight: "10px" }}
+          />
+          Random
+        </Button>
+      </div>
     </div>
   );
 }
