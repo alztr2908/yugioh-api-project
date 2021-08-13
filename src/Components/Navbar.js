c02cb337178336c4c25120d6da73689592af899d
Alzter Aquino <alztr2908@gmail.com>
2021-08-13 18:17:23 +0800

    finish styling navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,15 @@
-import { Anchor, Button } from "antd";
+import { Button, AutoComplete, Switch } from "antd";
 import logo from "./logo/logo.png";
+import back from "./logo/back.jpg";
 import "./stylesheets/Navbar.css";
 import { AiFillHome } from "react-icons/ai";
 import { BiCabinet } from "react-icons/bi";
 import { FaRandom } from "react-icons/fa";
 
-const { Link } = Anchor;
-
 function Navbar() {
   return (
     <div className="background">
-      <img src={logo} alt="img" />
+      <img src={logo} alt="img" id="logo" />
       <div className="list-background">
         <Button className="list-background-button">
           <AiFillHome
@@ -30,6 +29,18 @@ function Navbar() {
           />
           Random
         </Button>
+      </div>
+      <div className="list-search">
+        <div className="list-search-container">
+          <img id="searchLogo" src={back} />
+        </div>
+        <AutoComplete
+          style={{ width: 450 }}
+          placeholder="input here"
+        ></AutoComplete>
+      </div>
+      <div>
+        <Switch />
       </div>
     </div>
   );
